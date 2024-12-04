14d69534706c3738d6e8abccb56bafa49c6e9a44
Hyeunggeun-Yun <gudrms121712@naver.com>
2024-12-05 08:40:48 +0900

    33번 답 및 해설? 추가

--- 33.js
+++ 33.js
@@ -34,9 +34,12 @@
 // 27	1, 3, 9, 27	                     4
 // 따라서, 24 - 25 + 26 + 27 = 52를 return 해야 합니다.
 
+// ** 버러지 같은 내 정답 **
+// 쓸대없이 포문을 2번 사용했다... 생각해보니 푸시 하지않고 i를 
+// 그냥 나누면 되는데 굳이 1번부터 하겠다고 포문을 한번더 만든 블랙말랑카우였다...
 
 // function solution(left, right) {
-//     let answer = 0; // 사이의 값을 더하거나 뺀 수
+//     let answer = 0;
 //     let arr = [];
 //     let numResult = 0;
 //     for (let i = left; i <= right; i++) {
@@ -67,40 +70,33 @@
 // 약수의 개수가 짝수인 수는 더하고,
 // 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
 
+// 다른사람 풀이 보고 다시푼 답변
+
 function solution(left, right) {
     let answer = 0;
+
+    for (let i = left; i <= right; i++) {
+        let count = 0;
+        for (let j = 1; j <= i; j++) {
+            if (i % j === 0) count++;
+        }
+        if (count % 2) answer -= i;
+        else answer += i;
+    }
 
     return answer;
 }
 
+// 수학적 모범답한? > 제곱근이 정수이면 약수의 개수가 홀수이다.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
